f95bcc32f4f8ba70e072c469e92c530dde3aee79
Colm <colmbreen89@gmail.com>
2017-11-17 13:32:01 +0000

    Switch camera when decrypting caesar

---
+++ Kryptos/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    
+    Camera mainCamera;
+    public Camera caesarCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        mainCamera.enabled = true;
+        caesarCamera.enabled = false;
+    }
+
+    void Update()
+    {
+        if (GameObject.Find("Player").GetComponent<FirstPersonController>().isDecrytping)
+        {
+            caesarCamera.enabled = true;
+            mainCamera.enabled = false;
+        }
+        else
+        {
+            mainCamera.enabled = true;
+            caesarCamera.enabled = false;
+        }
+    }
+}
